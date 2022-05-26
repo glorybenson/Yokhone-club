6eec1a059a14998e22cd39858aefb01663bfef89
Glory Benson <glorybenson3738@gmail.com>
2022-05-26 21:08:59 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -34,14 +34,16 @@ use Illuminate\Support\Facades\Route;
 
 
 
-//Route::redirect('/', '/en');
+Route::redirect('/', '/en');
 
-Route::group(['prefix' => '{language}'], function () {
+
 
     Route::get('/', function () {
         return redirect()->route('login');
         return view('welcome');
         });
+
+Route::group(['prefix' => '{language}'], function () {
 
 
     //Auth::routes();
