2f768f3d96a353111b79ea2c38a000305627fb0c
Glory Benson <glorybenson3738@gmail.com>
2022-05-26 20:34:01 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,11 @@ use Illuminate\Support\Facades\Route;
     
 });*/
 
+Route::get('/test', function () {
+    App::setLocale('fr');
+    dd(App::getLocale());
+});
+
 Route::get('/', function () {
     return redirect()->route('login');
     return view('welcome');
