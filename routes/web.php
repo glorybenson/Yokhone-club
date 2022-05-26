1fb86f82294a26d5f97b0ff2bd45b9c2942700ff
Glory Benson <glorybenson3738@gmail.com>
2022-05-26 21:02:38 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return ()->route('login');
     return view('welcome');
     });
 
